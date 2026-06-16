e291c6109846c93f6d26d48e69307fd8da6980e5
Diego Kaique <kaique_0208@hotmail.com>
2026-06-16 20:23:44 -0300

    Update and rename sql/01_data_exploration.sql to sql/sql/01_data_exploration.sql

---
+++ sql/sql/01_data_exploration.sql
@@ -0,0 +1,29 @@
+-- Exploring customers table
+
+SELECT *
+FROM olist_customers_dataset
+LIMIT 10;
+
+-- Exploring orders table
+
+SELECT *
+FROM olist_orders_dataset
+LIMIT 10;
+
+-- Exploring order items table
+
+SELECT *
+FROM olist_order_items_dataset
+LIMIT 10;
+
+-- Exploring payments table
+
+SELECT *
+FROM olist_order_payments_dataset
+LIMIT 10;
+
+-- Exploring sellers table
+
+SELECT *
+FROM olist_sellers_dataset
+LIMIT 10;
